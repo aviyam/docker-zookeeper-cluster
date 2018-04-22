08eae8ed8776390d73b4df11aef3668da1462e7b
Aviyam Fischer <aviyam@gmail.com>
2018-04-22 19:43:53 +0300

    Update docker-entrypoint.sh
    tidy and add logging on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,28 +2,22 @@
 
 set -e
 
-
-# Generate config if it doesn't exist
 if [[ ! -f "$CONF_DIR/zoo.cfg" ]]; then
     CONFIG="$CONF_DIR/zoo.cfg"
 
     echo "clientPort=$PORT" >> "$CONFIG"
     echo "dataDir=$DATA_DIR" >> "$CONFIG"
     echo "dataLogDir=$DATA_LOG_DIR" >> "$CONFIG"
-
     echo "tickTime=$TICK_TIME" >> "$CONFIG"
     echo "initLimit=$INIT_LIMIT" >> "$CONFIG"
     echo "syncLimit=$SYNC_LIMIT" >> "$CONFIG"
-
     echo "maxClientCnxns=$MAX_CLIENT_CNXNS" >> "$CONFIG"
-
     echo "autopurge.purgeInterval=$PURGE_INTERVAL" >> "$CONFIG"
     echo "autopurge.snapRetainCount=$SNAP_RETAIN_INTERVAL" >> "$CONFIG"
     
     IFS=', ' read -r -a ZOOKEEPER_SERVERS_ARRAY <<< "$ZOO_SERVERS"
     export ZOOKEEPER_SERVERS_ARRAY=$ZOOKEEPER_SERVERS_ARRAY
-    
-# now append information on every ZooKeeper node in the ensemble to the ZooKeeper config:
+
     for index in "${!ZOOKEEPER_SERVERS_ARRAY[@]}"
       do
         ZKID=$(($index+1))
@@ -36,8 +30,9 @@ if [[ ! -f "$CONF_DIR/zoo.cfg" ]]; then
      echo "server.$ZKID=$ZKIP:2888:3888" >> "$CONFIG"
 done
 fi
-
+echo "Starting zookeeper server node $ZOO_MY_ID"
+echo "with  the following configuration settings:"
+echo "$CONFIG"
 echo "${ZOO_MY_ID:-1}" > "$DATA_DIR/myid"
 
 exec "$@"
-
